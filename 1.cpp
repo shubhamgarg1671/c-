3c618f7b9eee35c2f484690786bf6844e9ee0b54
Shubham Garg <shubhamgarg1671@gmail.com>
2021-08-03 12:35:52 +0530

    build folder removed

--- 1.cpp
+++ 1.cpp
@@ -12,8 +12,8 @@ int main()
     //freopen("input.txt", "r", stdin);
     //freopen("text.txt", "w", stdout);
     ll t = 1;
-    //cin >> t;
-    while (t--) 
+    cin >> t;
+    while (t--)
     {
         solve();
     }
@@ -23,6 +23,24 @@ void solve()
 {
     ll n, i;
     cin >> n;
+    ll ar[2][n];
+    for (i = 0; i < n; i++)
+    {
+        cin >> ar[0][i];
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        cin >> ar[1][i];
+    }
+    ar[1][n - 1] = 0;
+    ar[0][0] = 0;
+    for (i = n - 2; i >= 0; i--)
+    {
+        ar[1][i] = ar[1][i + 1] + ar[1][i];
+    }
+    ll ans = LLONG_MAX;
     
+    cout << ans;
     cout << endl;
 }
