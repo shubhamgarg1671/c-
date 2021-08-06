a71c3b07049c5779b88b00bd48275829c37ef1b4
Shubham Garg <shubhamgarg1671@gmail.com>
2021-08-06 16:13:46 +0530

    long challenge stert

--- 1.cpp
+++ 1.cpp
@@ -4,13 +4,6 @@ using namespace std;
 #define lld long double
 #define pb push_back
 
-//this can sort 2d VECTOR based on column where compleete row moves together
-bool sortcol( const vector<ll>& v1, const vector<ll>& v2 ) { 
- return v1[1] < v2[1]; 
-} 
-
-
-
 void solve();
 int main()
 {
@@ -28,41 +21,8 @@ int main()
 
 void solve()
 {
-    ll n, i, k;
-    cin >> n >> k;
-    vector<vector <ll> > vec;
-    vector<ll> tpvec;
-    tpvec.push_back(0);
-    tpvec.push_back(0);
-    for (i = 0; i <n; i++)
-    {
-        tpvec[0] = i + 1;
-        cin >> tpvec[1];
-        vec.push_back(tpvec);
-    }
+    ll n, i;
+    cin >> n;
     
-    ll ans = LLONG_MIN;
-    for (i = 1; i < n; i++)
-    {
-        ll tp = (vec[i - 1][0] * vec[i][0]) - (k * (vec[i - 1][1] | vec[i][1]));
-        if (i > 1)
-        {
-            ll tp1 = (vec[i - 2][0] * vec[i][0]) - (k * (vec[i - 2][1] | vec[i][1]));
-            tp = max(tp, tp1);
-        }
-        ans = max(ans, tp);
-    }
-    sort(vec.begin(), vec.end(), sortcol);
-    for (i = 1; i < n; i++)
-    {
-        ll tp = (vec[i - 1][0] * vec[i][0]) - (k * (vec[i - 1][1] | vec[i][1]));
-        if (i > 1)
-        {
-            ll tp1 = (vec[i - 2][0] * vec[i][0]) - (k * (vec[i - 2][1] | vec[i][1]));
-            tp = max(tp, tp1);
-        }
-        ans = max(ans, tp);
-    }
-    cout << ans;
     cout << endl;
 }
