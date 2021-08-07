fd4fe233938b17a55004d0eb6a3adc70f3745c6f
Shubham Garg <shubhamgarg1671@gmail.com>
2021-08-07 15:42:06 +0530

    aug long special triplets

--- 1.cpp
+++ 1.cpp
@@ -4,47 +4,76 @@ using namespace std;
 #define lld long double
 #define pb push_back
 
-void solve();
+ll N = 100001;
+ll ar[100001];
+vector <vector<ll> >vec(100001); 
+void allFactor()
+{
+    ll i, j;
+    for (i = 2; i < N; i++)
+    {
+        vec[i].push_back(1);
+        for (j = 2; j <= sqrt(i); j++)
+        {
+            if (i % j == 0)
+            {
+                vec[i].push_back(j);
+                if (j * j != i)
+                vec[i].push_back(i / j);
+            }
+        }
+        sort(vec[i].begin(), vec[i].end());
+    }
+}
+
+void solve1(ll n);
+ll solve(ll n);
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     //freopen("input.txt", "r", stdin);
-    //freopen("text.txt", "w", stdout);
+    freopen("text.txt", "w", stdout);
     ll t = 1;
     cin >> t;
+    allFactor();
     while (t--)
     {
-        solve();
+        solve1(t);
     }
 }
 
-void solve()
+void solve1(ll n)
 {
-    ll n, i, p, k;
-    cin >> n >> p >> k;
+    ll  i;
     ll ans = 0;
-    if (n % k == 0)
+    for (i = 2; i <= n; i++)
     {
-        ans = p % k;
-//        ans--;
-        ans = ans * (n / k);
+        ll tp = n / i;
+        ans = ans + (tp * vec[i].size());
+//        cout << ans << " ";
+        tp = n % i;
+        tp = (upper_bound(vec[i].begin(), vec[i].end(), tp) - vec[i].begin());
+        ans = ans + tp;
+  //      cout << ans << "  ";
     }
-    else
+    if (ans != solve(n))
+    cout << n << endl;
+}
+
+ll solve(ll n)
+{
+    ll i;
+    ll ans = 0;
+    for (i = 1; i <= n; i++)
     {
-        ans = p % k;
-//        ans--;
-        ans = ans * ((n / k) + 1);  
+        for (ll j = 1; j <= n; j++)
+        {
+            if (i % j != 0 && j % (i % j) == 0)
+            {
+                ans++;
+            }
+        }
     }
-    ans = ans + (p / k);
-    if (((n - 1) % k) < p % k)
-    {
-//        cout << "ef";
-        ll tp = (n - 1) % k;
-        ll tp1 = p % k;
-        tp1--;
-        ans = ans - (tp1 - tp);
-    }
-    cout << ans + 1;
-    cout << endl;
+    return ans;
 }
--- 3.cpp
+++ 3.cpp
@@ -4,16 +4,39 @@ using namespace std;
 #define lld long double
 #define pb push_back
 
+ll N = 100001;
+ll ar[100001];
+vector <vector<ll> >vec(100001); 
+void allFactor()
+{
+    ll i, j;
+    for (i = 2; i < N; i++)
+    {
+        vec[i].push_back(1);
+        for (j = 2; j <= sqrt(i); j++)
+        {
+            if (i % j == 0)
+            {
+                vec[i].push_back(j);
+                if (j * j != i)
+                vec[i].push_back(i / j);
+            }
+        }
+        sort(vec[i].begin(), vec[i].end());
+    }
+}
+
 void solve();
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     //freopen("input.txt", "r", stdin);
-    //freopen("text.txt", "w", stdout);
+    //freopen("text1.txt", "w", stdout);
     ll t = 1;
     cin >> t;
-    while (t--) 
+    allFactor();
+    while (t--)
     {
         solve();
     }
@@ -21,8 +44,22 @@ int main()
 
 void solve()
 {
-    ll n, i, t;
+    ll n, i;
     cin >> n;
-    cout << n;
+    ll ans = 0;
+    for (i = 2; i <= n; i++)
+    {
+        ll tp = n / i;
+        ans = ans + (tp * vec[i].size());
+        cout << "i = " << i << " " << (tp * vec[i].size());
+        tp = n % i;
+        cout << " n %/ i "<<tp ;
+        tp = (upper_bound(vec[i].begin(), vec[i].end(), tp) - vec[i].begin());
+        cout << " "<< vec[i][tp];
+        cout << " " << tp;
+        ans = ans + tp;
+        cout << " " << ans << endl;
+    }
+    cout << ans;
     cout << endl;
 }
